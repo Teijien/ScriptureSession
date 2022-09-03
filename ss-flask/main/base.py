40bdad9c0b913ac53c59f683d60ed8927cf6a6f9
Teijien <tjhansen@ucsc.edu>
2022-09-03 03:41:00 -0700

    React Router basic route setup

--- ss-flask/main/base.py
+++ ss-flask/main/base.py
@@ -1,15 +1,17 @@
+from distutils.command.config import LANG_EXT
 from flask import Flask, render_template
 
 import TextScraper as Scraper
 import PageRequester as Requester
 
-SCRIPTURE_LIST = "https://www.churchofjesuschrist.org/study/scriptures?lang=eng"
+SCRIPTURE_LIST = "https://www.churchofjesuschrist.org/study/scriptures"
+URL_LANG = "?lang=eng"
 
 api = Flask(__name__, template_folder="../templates")
 
-@api.route("/images")
+@api.route("/sections")
 def get_image():
-    page = Requester.request_page(SCRIPTURE_LIST)
+    page = Requester.request_page(SCRIPTURE_LIST + URL_LANG)
     verses = Scraper.verse_parser(page, 'html.parser', 'div', "verse", 'a', 'sup', "body")
 
     return render_template('chapter.html', verses=verses)
--- ss-react/src/App.js
+++ ss-react/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import NavBar from './Components/NavItem.js'
 import View from './Components/View.js'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
-      <View />
+      <Routes>
+        <Route path='/' element={<View />} />
+      </Routes>
     </div>
   );
 }
